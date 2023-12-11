5f26b6ec8a00cb4da8dacdd8319618fb25cbd7cb
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-11 10:29:01 +0100

    Modified  test.bash

--- test.bash
+++ test.bash
@@ -39,3 +39,4 @@ repertoire(ocatl calc), else literal
   -b: usermod -g tekup -aG users user2
 5 -a: ACL(lil others(ken mich owner wila group proprietaire donc ACL), ynajm ygoulhelkch) setfacl -m u:user1:rw /etc/hosts
   -b: setfacl -R(old) -d(new) -m(9bal el m) g:tekup:rwx /home/tekup
+      setfacl -x(delete an facl) g:tekup /home/tekup
