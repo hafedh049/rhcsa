b99f80188fad2af8cec50c9d424e6033fd5fecc4
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-11 09:58:15 +0100

    Modified  test.bash

--- test.bash
+++ test.bash
@@ -15,4 +15,4 @@ QCM
 8 - c
 9 - chown :users a.out / chgrp users a.out
 10 - find / -name "*.sql"(el * mahich obligatoire)
-11 - SUID,SGID,SB(4,2,1) chmod 2755 sync.sh / chmod g+s sync.sh
+11 - SUID,SGID,SB(4,2,1) chmod 2755 sync.sh / chmod(cahnge moderators) g+s sync.sh
